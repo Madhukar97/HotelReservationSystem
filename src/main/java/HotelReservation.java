16a75372321e410a7311e3e32d3efb68fc7decdb
unknown <madhukar.tatiraju@gmail.com>
2021-11-16 08:44:00 +0530

    UC1 added

--- src/main/java/HotelReservation.java
+++ src/main/java/HotelReservation.java
@@ -1,5 +1,31 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class HotelReservation {
+
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Hotel Reservation System");
+
+        Map<String, Hotel> hotelReservation = new HashMap<>();
+        Hotel Lakewood = new Hotel();
+        Hotel Bridgewood = new Hotel();
+        Hotel Ridgewood = new Hotel();
+
+        hotelReservation.put("Lakewood", Lakewood);
+        hotelReservation.put("Bridgewood", Bridgewood);
+        hotelReservation.put("Ridgewood", Ridgewood);
+
     }
+}
+
+class Hotel {
+
+    String hotelName;
+    int rating;
+    String dayType;
+    int rate;
+    String customer_type;
+
+
 }
