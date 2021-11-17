91afa855e5d00e4e106eb479c6ad33128b2a14ef
unknown <madhukar.tatiraju@gmail.com>
2021-11-17 19:42:03 +0530

    UC3 and TC3 updated

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,27 +1,29 @@
-import junit.framework.TestCase;
+
 import org.junit.Assert;
+import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
+public class HotelReservationTest {
 
-public class HotelReservationTest extends TestCase {
-
-    Map<String, Hotel> hotelReservation = new HashMap<>();
-
-
-    public void testAddHotel() {
-
+    @Test
+    public void givenHotelNamesWhenAddedShouldReturnSize() {
         HotelReservation obj = new HotelReservation();
-        Hotel obj1 = new Hotel();
-        Hotel obj2 = new Hotel();
-        hotelReservation.put("Lakewood",obj1);
-        hotelReservation.put("Bridgewood",obj2);
-        obj1.setHotelName("Lakewood");
-        obj1.setRate(100);
-        obj2.setHotelName("Bridgewood");
-        obj2.setRate(40);
-        int cheapestRate = obj2.getRate()*2;
-        Assert.assertEquals(80,cheapestRate);
+        obj.addHotel();
+        Assert.assertEquals(3, obj.hotelReservation.size());
     }
 
+    @Test
+    public void givenDateRangeShouldReturnTheCheapestHotelRate() {
+        HotelReservation obj = new HotelReservation();
+        obj.addHotel();
+        Assert.assertEquals(220, obj.findCheapestHotel("2020-09-10", "2020-09-11"));
+    }
+
+    @Test
+    public void givenWeekDayWeekEndRatesShouldReturnThoseRates() {
+        HotelReservation obj = new HotelReservation();
+        obj.addHotel();
+        Assert.assertEquals(90, obj.hotelReservation.get("Lakewood").getWeekendRate());
+        Assert.assertEquals(50, obj.hotelReservation.get("Bridgewood").getWeekendRate());
+        Assert.assertEquals(150, obj.hotelReservation.get("Ridgewood").getWeekendRate());
+    }
 }
